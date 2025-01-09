74e71f6fa4ae66c103c734e50d988659082f0344
Kaan Latinler <kaanlatinlerhd@gmail.com>
2025-01-10 01:05:48 +0300

    change something

--- src/routes/projectRoutes.js
+++ src/routes/projectRoutes.js
@@ -10,9 +10,9 @@ const {
   getProjectByName,
 } = require("../controllers/projectController");
 const authenticateToken = require("../middlewares/authMiddleware");
-router.get("/get-all-projects",  getProjects);
+router.get("/get-all-projects", getProjects);
 router.get("/get-project-by-id/:id", authenticateToken, getProjectById);
-router.get("/get-project-by-name/:name", authenticateToken, getProjectByName);
+router.get("/get-project-by-name/:name", getProjectByName);
 router.post("/create-project", authenticateToken, createProject);
 router.put("/update-project/:id", authenticateToken, updateProject);
 router.delete("/delete-project/:id", authenticateToken, deleteProject);
